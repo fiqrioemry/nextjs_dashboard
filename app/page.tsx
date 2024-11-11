f23bd6967e759d0263affacb56321d201c93d574
fiqrioemry <foemry@gmail.com>
2024-11-11 09:53:17 +0700

    changing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <section>
+    <section className="bg-blur bg-black/20">
       <div className="container mx-auto flex-center h-screen">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam quod
-          nulla dolore nostrum non. Quia aut cupiditate voluptas rem facere quod
-          similique pariatur placeat accusamus recusandae, possimus, libero
-          officiis atque!
-        </p>
+        <div className="w-[300px] bg-white rounded-md space-y-4 text-center py-4 px-4">
+          <h1>Web store management</h1>
+          <form action="submit" className="space-y-4">
+            <input
+              type="text"
+              className="w-full px-2 py-2 rounded-md bg-foreground"
+              placeholder="email"
+            />
+            <input
+              type="text"
+              className="w-full px-2 py-2 rounded-md bg-foreground"
+              placeholder="password"
+            />
+
+            <Button className="w-full">Login</Button>
+            <Button className="w-full">Login with google</Button>
+          </form>
+        </div>
       </div>
     </section>
   );
